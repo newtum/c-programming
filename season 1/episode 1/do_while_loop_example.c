b0d4717d819370f01ac36b60485bb1203cdc436a
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-11 16:37:40 +0530

    initial commit

---
+++ season 1/episode 1/do_while_loop_example.c
@@ -0,0 +1,17 @@
+// Soruce code is provided by Newtum Solutions Private Limited for more details please visit to www.newtum.com
+
+// For Details explaination of the code please watch the video first and then execute the examples
+
+// Video is availabe at Amazon, YouTube DVD
+
+// example of do while loop
+
+main()
+{
+ 	int i=20;
+	do
+	{
+		printf(" %d", i);
+		i++;
+	}while (i<10);
+}
