09baba2b18f64fe45302c7cec12570d0cec316fc
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-11 16:26:39 +0530

    initial commit

---
+++ season 1/episode 1/print_stars_in_a_row.c
@@ -0,0 +1,17 @@
+// Soruce code is provided by Newtum Solutions Private Limited for more details please visit to www.newtum.com
+
+// For Details explaination of the code please watch the video first and then execute the examples
+
+// Video is availabe at Amazon, YouTube DVD
+
+// write a small program to print 10 stars in a row
+
+
+main()
+{
+    int j;
+    for (j=1; j <= 10; j++)
+    {
+        printf("*");
+    }
+}
