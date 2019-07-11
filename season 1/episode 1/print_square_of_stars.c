0b127dd46dae16b02b4b51843eff5102ed6935fa
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-11 16:30:30 +0530

    initial commit

---
+++ season 1/episode 1/print_square_of_stars.c
@@ -0,0 +1,22 @@
+// Soruce code is provided by Newtum Solutions Private Limited for more details please visit to www.newtum.com
+
+// For Details explaination of the code please watch the video first and then execute the examples
+
+// Video is availabe at Amazon, YouTube DVD
+
+// print square of stars
+
+
+main()
+{
+    int i, j;
+    for (j=1; j <= 10; j++)
+    {
+        for (i=1; i<= 10; i++)
+        {
+             printf("*");
+        }
+        printf("\n");
+
+    }
+}
