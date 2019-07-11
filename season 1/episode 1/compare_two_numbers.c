1e300d504b7d0e40e670c14d3a5e93062d035e0d
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-11 18:27:45 +0530

    initial commit

---
+++ season 1/episode 1/compare_two_numbers.c
@@ -0,0 +1,19 @@
+// Soruce code is provided by Newtum Solutions Private Limited for more details please visit to newtum.com
+
+// For Details explaination of the code please watch the video first and then execute the examples
+
+// Video is availabe at Amazon, YouTube DVD
+
+// write the program to compare- two numbers .
+
+main()
+{
+    int A, B;
+    printf("Please Enter value for A and B : ");
+    scanf("%d %d",&A,&B);
+
+    if(A > B)
+        printf("A is Greater than B");
+    else
+        printf("A is Less than B");
+}
