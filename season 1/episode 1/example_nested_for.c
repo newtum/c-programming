44204bcc2c8abbd6eb051ab25190b0cef2c27b63
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-11 16:34:12 +0530

    initial commit

---
+++ season 1/episode 1/example_nested_for.c
@@ -0,0 +1,21 @@
+// Soruce code is provided by Newtum Solutions Private Limited for more details please visit to www.newtum.com
+
+// For Details explaination of the code please watch the video first and then execute the examples
+
+// Video is availabe at Amazon, YouTube DVD
+
+// example of nested for .
+
+
+main()
+{
+     int i, j;
+     for (j=1; j <= 10; j++)
+        {
+            for (i=1; i<= j; i++)
+             {
+                 printf("*");
+     }
+     printf("\n");
+     }
+}
