82da5296601bf080428a69d08de1c52f4f1f18b5
manojkolhe <manojkumar.kolhe@techaroha.com>
2019-07-17 11:47:45 +0530

    initial commit

--- season 1/episode 1/prime_number.c
+++ season 1/episode 1/prime_number.c
@@ -8,27 +8,23 @@
 
 main()
 {
-  int n, i, count = 0;
-    printf("Enter a number : ");
-    scanf("%d", &n);
-    for(i = 2; i <= n/2; ++i)
+    int i, number;
+    int mod;
+    printf("Determine whether a number is prime or not\n");
+    printf("Enter value for Number : ");
+    scanf("%d", &number);
+
+    for(i=2 ; i<number; i++)
     {
-        // condition for nonprime number
-        if(n%i == 0)
-        {
-            count = 1;
-            break;
-        }
+        mod=number%i;
+        if(mod == 0)
+         {
+           printf("%d is Not a Prime number.\n",number);
+             break;
+         }
     }
-    if (n == 1)
+    if(number==i)
     {
-      printf("1 is neither a prime nor a composite number.");
-    }
-    else
-    {
-        if (count == 0)
-          printf("%d is a prime number.", n);
-        else
-          printf("%d is not a prime number.", n);
+        printf("%d is a Prime number.", number);
     }
 }
